0502a6ca45a5f42f434e7c07ca31e2869c7d10b8
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2025-07-05 21:55:10 +0200

    Initial commit

---
+++ src/main/java/mohammad/fullstack/ai/ecommercetelusko/ECommerceTeluskoApplication.java
@@ -0,0 +1,13 @@
+package mohammad.fullstack.ai.ecommercetelusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceTeluskoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceTeluskoApplication.class, args);
+    }
+
+}
