2a779bad861c8825459de24b6df851f079e9e8a4
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2025-07-05 22:52:53 +0200

    Omg I love javadoc rendering

--- src/main/java/mohammad/fullstack/ai/ecommercetelusko/ECommerceTeluskoApplication.java
+++ src/main/java/mohammad/fullstack/ai/ecommercetelusko/ECommerceTeluskoApplication.java
@@ -3,6 +3,10 @@ package mohammad.fullstack.ai.ecommercetelusko;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is where I am making this decision for my career.
+ * I want and will be a fullstack AI developer in Germany.
+ */
 @SpringBootApplication
 public class ECommerceTeluskoApplication {
 
